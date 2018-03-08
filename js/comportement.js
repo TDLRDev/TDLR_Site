016577950f825a03eea30ce03d1407b27d8ec1f9
RoiDesPerdants <alexandre.vives@gmail.com>
2018-03-08 13:21:50 +0100

    teste sur le js et finalisation du menu.

--- js/comportement.js
+++ js/comportement.js
@@ -2,9 +2,22 @@ $(document).ready(init);
 
 function init()
 {
+    
     $('#fullpage').fullpage({
         verticalCentered: false,
         anchors: ['homePage', 'pageJeu', 'pageNews', 'pageHistoire', 'PageNous'],
 	    menu: '#myMenu'
+    });
+    
+   /* $('#niveauUn li').on('click', function() {
+        $('#photo h2').text($('a', this).attr('title'));
+        $('#big_pict').attr('src', $('a', this).attr('href'));
+        $('#big_pict').attr('alt', $('a img', this).attr('alt'));
+    });*/
+    $('#toto').on('click', function() {
+        //$('#photo h2').text($('a', this).attr('title'));
+        $('#description img').first().attr('class', 'imageMinInactive');
+        $('#description img:nth-child(2)').attr('class', 'imageMinActive');
+        
     });
 }
