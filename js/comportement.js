92ef29503258e49d287fa4ef87a9b24fb5cc2959
RoiDesPerdants <alexandre.vives@gmail.com>
2018-03-22 19:10:08 +0100

    le js pour les images fonctionnent

--- js/comportement.js
+++ js/comportement.js
@@ -10,32 +10,13 @@ function init()
     });
     
     
-    // au clique sur la section le text et les images changent
-    $('#niveauUn li').click(function(){
+    $('.niveauDeux li').click(function(e){
         var to_show = $(this).attr('desc'); // je recupere le nom de reference qui sera commum a un texte, une image 
-        //var maListe = $('#menuDescription');
         
-        $('#description article img').each(function(){
-            if ($(this).attr('desc') == to_show) 
-            {
-                $(this).attr('class', 'imageMinActive');
-            }
-            else
-            {
-                $(this).attr('class', 'imageMinInactive');    
-            }
-        });
+        $('img[desc="'+to_show+'"').attr('class', 'imageMinActive');
+        $('img[desc="'+to_show+'"').siblings().attr('class', 'imageMinInactive');
         
-        $('#description article p').each(function(){
-            if ($(this).attr('desc') == to_show) 
-            {
-                $(this).attr('class', 'textActif');
-            }
-            else
-            {
-                $(this).attr('class', 'textInactif');    
-            }
-        });
-        
-    });  
+        $('p[desc="'+to_show+'"').attr('class', 'textActif');
+        $('p[desc="'+to_show+'"').siblings().attr('class', 'textInactif');
+    });    
 }
